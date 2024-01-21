6145fd6259d19d0b029160135d5ee8edad6bcc18
Kynan Ware <47394200+BagToad@users.noreply.github.com>
2024-01-21 15:47:01 -0700

    Add new image data and socials data placeholders (#2)
    * Add debug statement and comment to get data from response
    
    * Add camera and location data to available placeholders
    
    * Create socials string placeholder
    
    * refactor socialstring
    
    * fixed socialstring reduce function
    
    * Fix reduce function parameter order
    
    * Refactor SOCIALS array to filter out null values
    
    * Update social media links in index.js
    
    * Refactor social data variables in index.js
    
    * Fix incorrect override of PORTFOLIOURL
    
    * Add socials to placeholders in README
    
    ---------
    
    Co-authored-by: Sukhpreet-s <41968447+Sukhpreet-s@users.noreply.github.com>

--- index.js
+++ index.js
@@ -40,18 +40,72 @@ unsplash.photos.getRandom({
     query: QUERY,
 })
     .then(data => {
+        core.debug(data)
+
+        // Get data from response
         const URL = data.response.urls.regular;
         const DESC = data.response.description;
         const ALTDESC = data.response.alt_description;
+
+        // Social data
         const NAME = data.response.user.name;
-        const PORTFOLIOURL = data.response.user.portfolio_url;
+        const PORTFOLIOURL = data.response.user.social.portfolio_url;
+        const INSTAGRAM = data.response.user.social.instagram_username 
+            ? `[instagram](https://instagram.com/${data.response.user.social.instagram_username})` 
+            : '';
+
+        const PORTFOLIO = data.response.user.social.portfolio_url 
+            ? `[portfolio](${data.response.user.social.portfolio_url})` 
+            : '';
+
+        const TWITTER = data.response.user.social.twitter_username 
+            ? `[twitter](https://twitter.com/${data.response.user.social.twitter_username})` 
+            : '';
+
+        const PAYPAL = data.response.user.social.paypal_email 
+            ? `[paypal](mailto:${data.response.user.social.paypal_email})` 
+            : '';
+        const SOCIALS = [INSTAGRAM, PORTFOLIO, TWITTER, PAYPAL].filter(Boolean);
+
+        // Construct a string that looks like this: "instagram / portfolio / twitter"
+        let SOCIALSSTRING = SOCIALS.reduce((finalStr, ele, index, arr)=> {
+            if (index < arr.length - 1) {
+                return finalStr + ele + " / ";
+            } else {
+                return finalStr + ele;
+            }
+        }, "");
+
+        // Camera/EXIF data
+        const MODEL = data.response.exif.model;
+        const EXPOSURETIME = data.response.exif.exposure_time;
+        const APERTURE = data.response.exif.aperture;
+        const FOCALLENGTH = data.response.exif.focal_length;
+        const ISO = data.response.exif.iso;
+
+        // Location data
+        const LOCATION = data.response.location.name;
+        const COUNTRY = data.response.location.country;
+        const LATITUDE = data.response.location.position.latitude;
+        const LONGITUDE = data.response.location.position.longitude;
 
         // Replace variables in template
         const result = templateFile.replace(/{{ unsplash-url }}/g, URL)
             .replace(/{{ unsplash-description }}/g, DESC)
             .replace(/{{ unsplash-alt-description }}/g, ALTDESC)
             .replace(/{{ unsplash-name }}/g, NAME)
-            .replace(/{{ unsplash-portfolio-url }}/g, PORTFOLIOURL);
+            .replace(/{{ model }}/g, MODEL)
+            .replace(/{{ exposure-time }}/g, EXPOSURETIME)
+            .replace(/{{ aperture }}/g, APERTURE)
+            .replace(/{{ focal-length }}/g, FOCALLENGTH)
+            .replace(/{{ iso }}/g, ISO)
+            .replace(/{{ location }}/g, LOCATION)
+            .replace(/{{ country }}/g, COUNTRY)
+            .replace(/{{ latitude }}/g, LATITUDE)
+            .replace(/{{ longitude }}/g, LONGITUDE)
+            .replace(/{{ unsplash-portfolio-url }}/g, PORTFOLIOURL)
+            .replace(/{{ socials }}/g, SOCIALSSTRING);
+
         // Write processed template to README.md
         try {
             fs.writeFileSync('README.md', result);
