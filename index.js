95eccbd8819ca0926cd5ccda731bb796379fbbca
Kynan Ware <47394200+BagToad@users.noreply.github.com>
2024-01-29 15:32:24 +0000

    Refactor geoJSON string to include correct code block

--- index.js
+++ index.js
@@ -95,7 +95,8 @@ unsplash.photos.getRandom({
         // Construct a geoJSON string from the location data.
         // This is used to display the location on a map in the README.md
         const MAPZOOM = 0.3;
-        const GEOJSON = `{
+        const GEOJSON = `\`\`\`geojson
+        {
             "type": "FeatureCollection",
             "features": [
                 {
@@ -140,7 +141,8 @@ unsplash.photos.getRandom({
                     }
                 }
             ]
-        }`;
+        }
+        \`\`\``;
 
         // Replace variables in template
         const result = templateFile
